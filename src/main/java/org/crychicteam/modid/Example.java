e06da2116613bb3d493a58b2d0dd4b7bfd3d7055
M1hono <xche0557@uni.sydney.edu.au>
2024-09-11 00:29:29 +1000

    Add Registrate and update Plugin format.

--- src/main/java/org/crychicteam/modid/Example.java
+++ src/main/java/org/crychicteam/modid/Example.java
@@ -1,13 +1,12 @@
-package org.zeith.modid;
+package org.crychicteam.modid;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(SteelRiptide.MOD_ID)
-public class SteelRiptide
+@Mod(Example.MOD_ID)
+public class Example
 {
-	public static final String MOD_ID = "steelriptide";
-
+	public static final String MOD_ID = "templatemode";
 	public static ResourceLocation id(String path)
 	{
 		return new ResourceLocation(MOD_ID, path);
