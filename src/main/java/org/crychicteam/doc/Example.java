ee1a56d9777bf7a1ecdb3b4e3eb4fe86f8cd58c7
M1hono <xche0557@uni.sydney.edu.au>
2024-10-18 13:48:23 +1100

    Add Player Animator as optional dependencies.

--- src/main/java/org/crychicteam/doc/Example.java
+++ src/main/java/org/crychicteam/doc/Example.java
@@ -1,4 +1,4 @@
-package org.crychicteam.modid;
+package org.crychicteam.doc;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(Example.MOD_ID)
 public class Example
 {
-	public static final String MOD_ID = "templatemode";
+	public static final String MOD_ID = "doc";
 	public static ResourceLocation id(String path)
 	{
 		return new ResourceLocation(MOD_ID, path);
