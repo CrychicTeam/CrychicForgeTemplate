6cc913651b6ef0866cde05a0c6698bc04e54c910
M1hono <xche0557@uni.sydney.edu.au>
2024-11-19 04:49:54 +1100

    Fix Mixin refmap.

---
+++ src/main/java/org/crychicteam/modid/mixins/ExampleMixin.java
@@ -0,0 +1,8 @@
+package org.crychicteam.modid.mixins;
+
+import net.minecraft.world.entity.vehicle.Minecart;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Minecart.class)
+public class ExampleMixin {
+}
