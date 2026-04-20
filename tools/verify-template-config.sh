8200b02f2470957cbcdd01157f298bbf6efd5179
M1hono <xche0557@uni.sydney.edu.au>
2026-04-21 05:33:11 +1000

    feat: add template toml config loading

--- tools/verify-template-config.sh
+++ tools/verify-template-config.sh
@@ -8,3 +8,6 @@ test -f template.toml
 test ! -f build.txt
 bash ./gradlew help --no-daemon > /tmp/pickaid-template-help.log 2>&1
 rg "BUILD SUCCESSFUL" /tmp/pickaid-template-help.log
+bash ./gradlew properties --no-daemon > /tmp/pickaid-template-properties.log 2>&1
+rg "^version: 0.0.1$" /tmp/pickaid-template-properties.log
+rg "^group: com.mihono.pickaid$" /tmp/pickaid-template-properties.log
